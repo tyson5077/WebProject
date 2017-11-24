5c866914698b9b7bc24849cfd4051315a1c3282b
tyson5077 <33962953+tyson5077@users.noreply.github.com>
2017-11-24 23:28:50 +0500

    Web Phase 1

---
+++ custom.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+  function initMap() {
+        var uluru = {lat: 31.5215335, lng: 74.3197554};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 18,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      } 
